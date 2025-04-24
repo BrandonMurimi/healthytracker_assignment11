f24a186e9e93d62f32fd3b203c00617f04ea0b69
BrandonMurimi <Brandon26Charis@gmail.com>
2025-04-24 02:38:13 +0200

    Update repository_factory.rs

--- factories/repository_factory.rs
+++ factories/repository_factory.rs
@@ -1,1 +1,20 @@
+use crate::repositories::{
+    UserRepository, 
+    inmemory::InMemoryUserRepository
+};
 
+pub enum StorageType {
+    Memory,
+    // FileSystem,
+    // Database,
+}
+
+pub struct RepositoryFactory;
+
+impl RepositoryFactory {
+    pub fn user_repository(storage_type: StorageType) -> Box<dyn UserRepository> {
+        match storage_type {
+            StorageType::Memory => Box::new(InMemoryUserRepository::new()),
+        }
+          }
+}
